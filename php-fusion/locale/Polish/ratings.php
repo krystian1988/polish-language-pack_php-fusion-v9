0564455ed49f15f70108221bb109a842b7f66065
krystian1988 <39311008+krystian1988@users.noreply.github.com>
2018-05-17 09:26:53 +0200

    Ratings complete

--- php-fusion/locale/Polish/ratings.php
+++ php-fusion/locale/Polish/ratings.php
@@ -1,18 +1,27 @@
 <?php
-$locale['r100'] = "Ratings";
-$locale['r101'] = "No Ratings have been Posted.";
-$locale['r102'] = "Remove Rating";
-$locale['r103'] = "Post Rating";
-$locale['r104'] = "Rating is available to Members only.\n\nPlease [RATING_ACTION] to vote.";
-$locale['r105'] = "You rated this as %s on %s";
-$locale['r106'] = "Please select your Rating";
-$locale['r107'] = "Choose Rating:";
-$locale['r108'] = "No Votes";
-$locale['r109'] = "Vote";
-$locale['r110'] = "Votes";
+$locale['r100'] = "Oceny";
+$locale['r101'] = "Brak ocen. Może czas dodać swoją?";
+$locale['r102'] = "Usuń ocenę";
+$locale['r103'] = "Dodaj ocenę";
+$locale['r104'] = "Tylko zarejestrowani użytkownicy mogą oceniać zawartość strony..\n\nProszę [RATING_ACTION] żeby móc zagłosować.";
+$locale['r105'] = "Ocenione przez Ciebie na <b>%s</b> dnia %s";
+$locale['r106'] = "Oceń dany materiał";
+$locale['r107'] = "- nie wybrano oceny -";
+$locale['r108'] = "0 głosów";
+$locale['r109'] = "głos";
+$locale['r110'] = "głosów";
 // Rating Options
-$locale['r120'] = "Awesome!";
-$locale['r121'] = "Very Good";
-$locale['r122'] = "Good";
-$locale['r123'] = "Average";
-$locale['r124'] = "Poor";
+$locale['r120'] = "Świetne!";
+$locale['r121'] = "Bardzo dobre";
+$locale['r122'] = "Dobre";
+$locale['r123'] = "Średnie";
+$locale['r124'] = "Słabe";
+
+/* My own patch, if you don't want it - comment lines below.
+ * To moja własna łatka, jeśli nie chcesz z niej korzystać - zakomentuj poniższe linie
+ * Tomasz "jantom" Jankowski, Date: 11 04 2011 16:10 */
+require_once INCLUDES."output_handling_include.php";
+$locale['r110a'] = "głosy";
+replace_in_output("2 ".$locale['r110'],"2 ".$locale['r110a']);
+replace_in_output("3 ".$locale['r110'],"3 ".$locale['r110a']);
+replace_in_output("4 ".$locale['r110'],"4 ".$locale['r110a']);
