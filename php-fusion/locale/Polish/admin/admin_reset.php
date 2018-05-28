cac43275f8566b2610099e0833b737bbabb1fb39
krystian1988 <39311008+krystian1988@users.noreply.github.com>
2018-05-28 20:18:39 +0200

    Admin reset
    Admin Reset

--- php-fusion/locale/Polish/admin/admin_reset.php
+++ php-fusion/locale/Polish/admin/admin_reset.php
@@ -1,35 +1,45 @@
 <?php
-$locale['apw_title'] = "Administrator Password Reset";
-$locale['apw_desc'] = "Changes Administrator login/admin passwords";
-$locale['apw_admin1'] = "Administrator Reset";
-$locale['apw_400'] = "Administrator(s) to reset:";
-$locale['apw_401'] = "All Administrators";
-$locale['apw_402'] = "Only Super Administrators";
-$locale['apw_403'] = "Only Administrators";
-$locale['apw_404'] = "Justification for reset:";
-$locale['apw_405'] = "Reset both login and admin passwords";
-$locale['apw_406'] = "Reset Passwords";
-$locale['apw_407'] = "Password reset at ";
-$locale['apw_408'] = "Hello [USER_NAME],\n\nYour admin password has been reset at [SITEURL]:\n\n
-Admin Password: [NEW_ADMIN_PASS]\n\nThe reason for this reset is: [RESET_MESSAGE]\n\nRegards,\n[ADMIN]";
-$locale['apw_409'] = "Hello [USER_NAME],\nYour login and admin passwords have been reset at [SITEURL]:\n\n
-Login Password: [NEW_PASS]\nAdmin Password: [NEW_ADMIN_PASS]\n\nThe reason for this reset is: [RESET_MESSAGE]\n
-Regards,\n[ADMIN]";
-$locale['apw_410'] = "Reset Results";
-$locale['apw_411'] = "Update Admin Password.";
-$locale['apw_412'] = "Admin Password:";
-$locale['apw_415'] = "Reset Log";
-$locale['apw_416'] = "Log ID";
-$locale['apw_417'] = "Reset time";
-$locale['apw_418'] = "Responsible";
-$locale['apw_419'] = "Reset";
-$locale['apw_420'] = "Successfull";
-$locale['apw_421'] = "Reason";
-$locale['apw_422'] = "of";
-$locale['apw_423'] = "No reason given.";
-$locale['apw_424'] = "Admins reset:";
-$locale['apw_425'] = "Admins failed:";
-$locale['apw_426'] = "No password is modified.";
-$locale['apw_427'] = "Options";
-$locale['apw_428'] = "Do you realy want to delete this log?";
-$locale['apw_429'] = "Log deleted";
+$locale['apw_title'] = "Odnowienie hasła administratora";
+$locale['apw_desc'] = "Zmienia hasła użytkownika oraz administratora wybranym członkom administracji";
+$locale['apw_admin1'] = "Odnowienie hasła administratora";
+$locale['apw_400'] = "Konta do odnowienia hasła:";
+$locale['apw_401'] = "Wszyscy administratorzy";
+$locale['apw_402'] = "Tylko głównego administratora";
+$locale['apw_403'] = "Tylko administratorów";
+$locale['apw_404'] = "Powód odnowienia:";
+$locale['apw_405'] = "Odnów hasła użytkownika i administratora";
+$locale['apw_406'] = "Odnów hasła";
+$locale['apw_407'] = "Odnowiono administratora na ";
+$locale['apw_408'] = "Witaj [USER_NAME],
+Twoje hasło administratora na [SITEURL] zostało odnowione:
+Nowe hasło administratora: [NEW_ADMIN_PASS]
+Powód odnowienia: [RESET_MESSAGE]
+Z pozdrowieniami, [ADMIN]
+------
+Niniejsza wiadomość została wysłana automatycznie. Nie odpowiadaj na nią.";
+$locale['apw_409'] = "Witaj [USER_NAME],
+Twoje hasła użytkownika i administratora na [SITEURL] zostały odnowione:
+Nowe hasło użytkownika: [NEW_PASS]
+Nowe hasło administratora: [NEW_ADMIN_PASS]
+Powód odnowienia: [RESET_MESSAGE]
+Z pozdrowieniami, [ADMIN]
+------
+Niniejsza wiadomość została wysłana automatycznie. Nie odpowiadaj na nią.";
+$locale['apw_410'] = "Rezultat";
+$locale['apw_411'] = "Podano nieprawidłowe hasło administratora.";
+$locale['apw_412'] = "Hasło administratora:";
+$locale['apw_415'] = "Dziennik odnowień";
+$locale['apw_416'] = "Nr";
+$locale['apw_417'] = "Data";
+$locale['apw_418'] = "Kto";
+$locale['apw_419'] = "Odnowienie";
+$locale['apw_420'] = "Zakończone sukcesem";
+$locale['apw_421'] = "Powód";
+$locale['apw_422'] = "z";
+$locale['apw_423'] = "Nie podano powodu.";
+$locale['apw_424'] = "Resetowanie hasła administratorów:";
+$locale['apw_425'] = "Nie udało się zresetować hasła administratorom:";
+$locale['apw_426'] = "Żadne hasło nie zostało zmodyfikowane.";
+$locale['apw_427'] = "Opcje";
+$locale['apw_428'] = "Czy na pewno chcesz usunąć ten dziennik?";
+$locale['apw_429'] = "Dziennik został usunięty";
